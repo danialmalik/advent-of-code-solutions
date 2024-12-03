307cd3ce3b4a535af342bea5be9c28acacac2573
danialmalik <danialmalik321@gmail.com>
2024-12-03 11:35:31 +0500

    Day 03, Part 2

--- 2024/03/solution.py
+++ 2024/03/solution.py
@@ -4,19 +4,26 @@ import re
 
 def main():
     script_dir = os.path.dirname(__file__)
-    with open(f"{script_dir}/input.txt") as f:
-        lines = f.readlines()
 
-    lines = [line.strip() for line in lines]
+    sum_p1 = 0
+    sum_p2 = 0
 
-    sum = 0
-    for line in lines:
-        sum += parse_and_run_instruction(line)
+    enable_mul = True
 
-    print(sum)
+    with open(f"{script_dir}/input.txt", "r") as f:
+        for line in f:
+            sum_p1 += parse_and_run_instruction(line)
+
+            current_sum, enable_mul = parse_and_run_instruction_with_conditions(line, enable_mul)
+            sum_p2 += current_sum
+
+    print("Part-1: ",sum_p1)
+    print("Part-2: ",sum_p2)
+
 
 def parse_and_run_instruction(line: str):
     regex = "mul\((\d+),(\d+)\)"
+    regex = re.compile(regex)
 
     matches = re.findall(regex, line)
 
@@ -28,6 +35,45 @@ def parse_and_run_instruction(line: str):
         sum += a * b
 
     return sum
+
+# Part: Two
+
+def parse_and_run_instruction_with_conditions(line: str, enable_mul: bool = True):
+    """Use regex to parse the statement. Possible tokens are:
+    - mul(a, b): Multiply a by b
+    - do(): enable multiplication
+    - dont(): disable multiplication
+
+    The statement is string containing these tokens as well as many other characters
+    that should be ignored.
+
+    The function returns the sum of all multiplications and last state of the multiplication
+    flag (True if multiplication is enabled, False otherwise).
+    """
+    regex = "mul\((\d{1,3}),(\d{1,3})\)|(do\(\))|(don\'t\(\))"
+    regex = re.compile(regex)
+
+    matches = re.finditer(regex, line)
+
+    sum = 0
+    do_mul = enable_mul
+
+    for match in matches:
+
+        if match.group() == "do()":
+            do_mul = True
+        elif match.group() == "don't()":
+            do_mul = False
+
+        elif match.group().startswith("mul("):
+
+            a = int(match.group(1))
+            b = int(match.group(2))
+
+            if do_mul:
+                sum += a * b
+
+    return sum, do_mul
 
 
 
