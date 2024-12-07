59b8eec5e2bb03d8181baa6cc6e31585444a82f5
danialmalik <danialmalik321@gmail.com>
2024-12-07 12:31:00 +0500

    Day 07, Python, refactpr

--- 2024/07/solution.py
+++ 2024/07/solution.py
@@ -16,17 +16,26 @@ test_input = """190: 10 19
 def main():
     script_dir = os.path.dirname(__file__)
 
-    final_result = 0
+    final_result_p1 = 0
 
     with open(f"{script_dir}/input.txt", "r") as f:
+        lines = f.readlines()
+        print("Part-1: ", find_calibration_results(lines))
+        print("Part-2: ", find_calibration_results(lines, include_pipe=True))
 
-        # for line in test_input.splitlines():
-        for line in f:
+
+def find_calibration_results(lines, include_pipe=False):
+    final_result = 0
+
+    for line in lines:
 
             result, operands_str = line.split(': ')
             result = int(result)
             operands = list(map(int, operands_str.split(' ')))
             operators = ['+', '*']
+
+            if include_pipe:
+                operators.append('||')
 
             for op_permutation in itertools.product(operators, repeat=len(operands) - 1):
 
@@ -37,13 +46,14 @@ def main():
                         computed_result += operand
                     elif operator == '*':
                         computed_result *= operand
+                    elif operator == '||':
+                        computed_result = int(str(computed_result) + str(operand))
 
                 if computed_result == result:
                     final_result += result
                     break
 
-    print("Part-1: ", final_result)
-
+    return final_result
 
 if __name__ == "__main__":
     main()
